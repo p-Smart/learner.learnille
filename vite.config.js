72fbed857b696aa471b0841e741a6b8bcec5e6fc
Prince <princeayokunle2002@gmail.com>
2025-01-10 18:31:50 +0100

    feat: add calendar to overview page

--- vite.config.js
+++ vite.config.js
@@ -11,22 +11,22 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
   ],
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
-  publicDir: "public",
+  // build: {
+  //   outDir: "dist",
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes("node_modules")) {
+  //           return id
+  //             .toString()
+  //             .split("node_modules/")[1]
+  //             .split("/")[0]
+  //             .toString();
+  //         }
+  //       },
+  //     },
+  //   },
+  //   chunkSizeWarningLimit: 1000,
+  // },
+  // publicDir: "public",
 });
